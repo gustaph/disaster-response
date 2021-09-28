d4a0f58910adda069398f0619db70135331d6b11
Gustavo Soares <gustavosoares112@gmail.com>
2021-09-28 01:12:39 -0300

    ML pipeline

--- app/run.py
+++ app/run.py
@@ -1,36 +1,56 @@
 import json
 import plotly
 import pandas as pd
+import re
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
 
 def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
+    '''
+    function that transforms raw text into clean tokens
+    
+    Args:
+        - text: str -> text to be tokenized
+    Returns:
+        - token list
+    '''
+    
+    # regex obtained from Udacity course classes
+    url_regex = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
 
+    clean_text = re.sub(r'[^a-z-A-Z]|\W', ' ', text.lower()) # remove everything but characters
+    clean_text = re.sub(url_regex, 'urlplaceholder', clean_text) # replace url's by `urlplaceholder`
+    
+    tokens = word_tokenize(clean_text) # tokenize text
+    lemmatizer = WordNetLemmatizer() # instatiate lemmatizer
+    
     clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
+    for token in tokens:
+        clean_token = lemmatizer.lemmatize(token).strip() # lemmatize word
+        clean_token = lemmatizer.lemmatize(clean_token, pos='v') # lemmatize again operating by verbs
+        
+        if clean_token not in stopwords.words("english"): # remove stopwords
+            clean_tokens.append(clean_token)
+    
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('disaster_response', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,152 @@
 import sys
+import pandas as pd
+import numpy as np
+import re
+from sqlalchemy import create_engine
+from pickle import dump
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report
+from lightgbm import LGBMClassifier
+
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+def load_data(database_filepath: str):
+    '''
+    load the dataset from database
+    
+    Args:
+        - database_filepath: str -> database directory
+    Returns:
+        - X: np.array -> x values for modeling
+        - Y: np.array -> y values for modeling
+        - category_columns: list
+    '''
+    
+    engine = create_engine(f'sqlite:///{database_filepath}') # access data from database
+    df = pd.read_sql_table('disaster_response', engine) # get data from database
+
+    category_columns = df.drop(['id', 'message', 'original', 'genre'], axis=1).columns
+
+    X = df['message'].values
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis=1).values
+
+    return X, Y, category_columns
 
 
-def load_data(database_filepath):
-    pass
+def tokenize(text: str):
+    '''
+    function that transforms raw text into clean tokens
+    
+    Args:
+        - text: str -> text to be tokenized
+    Returns:
+        - token list
+    '''
+    
+    # regex obtained from Udacity course classes
+    url_regex = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
 
-
-def tokenize(text):
-    pass
+    clean_text = re.sub(r'[^a-z-A-Z]|\W', ' ', text.lower()) # remove everything but characters
+    clean_text = re.sub(url_regex, 'urlplaceholder', clean_text) # replace url's by `urlplaceholder`
+    
+    tokens = word_tokenize(clean_text) # tokenize text
+    lemmatizer = WordNetLemmatizer() # instatiate lemmatizer
+    
+    clean_tokens = []
+    for token in tokens:
+        clean_token = lemmatizer.lemmatize(token).strip() # lemmatize word
+        clean_token = lemmatizer.lemmatize(clean_token, pos='v') # lemmatize again operating by verbs
+        
+        if clean_token not in stopwords.words("english"): # remove stopwords
+            clean_tokens.append(clean_token)
+    
+    return clean_tokens
 
 
 def build_model():
-    pass
+    '''
+    build the ML model
+
+    Returns:
+        - ideal model based on GridSearch
+
+    > REFERENCES:
+    * Hyperparameter Tuning the Random Forest in Python [https://towardsdatascience.com/hyperparameter-tuning-the-random-forest-in-python-using-scikit-learn-28d2aa77dd74];
+    * Tuning the parameters of your Random Forest model [https://www.analyticsvidhya.com/blog/2015/06/tuning-random-forest-model/] 
+    '''
+
+    # first set of parameters based on references
+    PARAMS = { 
+        'learning_rate': 0.2,
+        'max_depth': 10,
+        'num_leaves': 20,
+        'feature_fraction': 0.6,
+        'n_estimators': 30,
+        'verbose': -1
+    }
+    
+    # model pipeline
+    pipeline = Pipeline([
+        ('count', CountVectorizer(tokenizer=tokenize)), # bag of words based on `tokenize` function
+        ('tfidf', TfidfTransformer()), # TF-IDF transformer
+        ('clf', MultiOutputClassifier(estimator=LGBMClassifier(**PARAMS), n_jobs=-1)), 
+    ])
+
+    PARAMS = {
+        'clf__estimator__subsample': (0.1, 0.2, 0.5),
+        'clf__estimator__class_weight': (None, 'balanced'),
+    }
+
+    grid_search = GridSearchCV(pipeline, param_grid=PARAMS) # operating GridSearch to find best parameters
+    return grid_search
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+def evaluate_model(model, X_test: np.array, Y_test: np.array, category_names: list):
+    '''
+    shows the overall ranking of each target column
+    
+    Args:
+        - model: classification model
+        - X_test: x values for test
+        - Y_test: y values for test
+        - category_names
+    Returns: all column classification report
+    '''
+    
+    y_pred = model.predict(X_test) # predict data
+    
+    for i, column in enumerate(category_names):
+        print(f"({column}):")
+        print(classification_report(Y_test[:,i], y_pred[:,i], zero_division=1))
+    
+    print("ACC:", (y_pred == Y_test).mean())
+        
+    return
 
 
-def save_model(model, model_filepath):
-    pass
+def save_model(model, model_filepath: str):
+    '''
+    export the model in pickle format
+    
+    Args:
+        - model: final model
+        - model_filepath: str -> directory from where to save the model
+    '''
+    
+    with open(model_filepath, 'wb') as file:
+        dump(model, file)
+
+    return
 
 
 def main():
